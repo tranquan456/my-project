72fc161291c7625722d3d0d0633b98322aa1dbc7
tranquan456 <quan17363@gmail.com>
2024-01-10 10:26:14 +0700

    b3

--- src/main.js
+++ src/main.js
@@ -1,2 +1,9 @@
-console.log ('a');
-console.log('b');
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.js"
+import classes from "./main.module.css";
+document.querySelector('#app').innerHTML=
+`
+<div class="${classes.red}">
+hello word
+</div>
+`;
---
+++ tailwind.config.cjs
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html","./components/*.{html,js}","./pages/*.{html,js}"],
+  prefix:"tw-",
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
